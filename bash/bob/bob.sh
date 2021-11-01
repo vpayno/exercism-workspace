786bce52f4688f1624e9050a0ae9f3c119f178ac
Victor Payno <vpayno@users.noreply.github.com>
2021-11-01 13:40:06 -0700

    bash/bob: 1st iteration

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,24 +1,144 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# Enable/Disable debug output.
+declare DEBUG="false"
+
+# Function: printf to stderr if DEBUG is set to true
+# Input : same as printf
+# Output: same as printf, except it goes to stderr instead of stdout
+# Return: from printf
+eprintf()
+{
+    # Pass all the printf arguments and redirect stdout to stderr.
+    # shellcheck disable=SC2059
+    ${DEBUG} && printf "$@" >&2
+}
+
+# Function: check the validity of the script's arguments.
+# Input : script arguments
+# Output: error messages
+# Return: 0 (valid) or >=1 (one or more input problems found)
+check_args()
+{
+    # Capture the script's arguments.
+    local -a vargs=( "$@" )
+
+    # Variable used to track the return codes from each of the checks.
+    local -i retval=0
+
+    # If there is no input, there will be silence (one of the tests).
+    # This breaks one of the tests that expects a response when nothing is spoken to Bob.
+    #if [[ ${#vargs[@]} -eq 0 ]]; then
+        #exit 0
+    #fi
+
+    # Was a single argument passed?
+    # This breaks one of the tests that expects a response when nothing is spoken to Bob.
+    # Change it to just test to see if we have too many arguments (forgot to quote the string?).
+    if [[ ${#vargs[@]} -gt 1 ]]; then
+        eprintf "ERROR: wrong number of arguments were passed [%s](%d)\n" "${vargs[*]}" "${#vargs[@]}"
+        show_usage
+        (( retval++ ))
+    fi
+
+    # If a non-zero value is returned, it means that one or more tests have failed.
+    return "${retval}"
+}
+
+# Function: print the scripts usage help message.
+# Input : none
+# Output: usage help screen
+# Return: 1 (always)
+show_usage()
+{
+    printf "Usage: %s \"<string>\"" "$0"
+}
+
+# Check the inputs for validity and exit if the checks fail.
+check_args "$@" || exit "$?"
+
+# Function: trim whitespace at the end of the string.
+# Input : string
+# Output: string with no leading or traling whitespace
+# Return: 0 (trimmed) or 1 (not trimmed)
+trim()
+{
+    local line="${1}"
+
+    # remove traling whitespace
+    line="${line#"${line%%[![:space:]]*}"}"
+
+    # remove leading whitespace
+    line="${line%"${line##*[![:space:]]}"}"
+
+    printf "%s" "${line}"
+}
+
+# Function: main function
+# Input : script argument(s)
+# Output: exercise output
+# Return: 0 (success) or >=1 (error)
+main()
+{
+    local input="${1}"
+
+    local -A responses
+    local response
+    local trigger
+
+    # Build a response assosiative array (dictionary).
+    responses["catchall"]="Fine. Be that way!"
+    responses["question"]="Sure."
+    responses["statement"]="Whatever."
+    responses["yelling"]="Whoa, chill out!"
+    responses["yelling_question"]="Calm down, I know what I'm doing!"
+
+    # Remove trailing white space.
+    input="$(trim "${input}")"
+
+    # Check the statement for clues on how we should respond.
+
+    # Look for a question with only numbers.
+    if [[ ${input} =~ ^([[:digit:]]|[[:punct:]]|[[:space:]]|\n|\r|\t)+\?[[:space:]]*$ ]]; then
+        trigger="question"
+
+    # Look for a statement with only numbers.
+    elif [[ ${input} =~ ^([[:digit:]]|[[:punct:]]|[[:space:]]|\n|\r|\t)+[!.]?[[:space:]]*$ ]]; then
+        trigger="statement"
+
+    # Look for a question in all caps.
+    elif [[ ${input} =~ ^([[:digit:]]|[[:upper:]]|[[:punct:]]|[[:space:]]|\n|\r|\t)+\?[[:space:]]*$ ]]; then
+        trigger="yelling_question"
+
+    # Look for a question.
+    elif [[ ${input} =~ ^([[:digit:]]|[[:alpha:]]|[[:punct:]]|[[:space:]]|\n|\r|\t)+\?[[:space:]]*$ ]]; then
+        trigger="question"
+
+    # Look for a statement in all caps.
+    elif [[ ${input} =~ ^([[:digit:]]|[[:upper:]]|[[:punct:]]|[[:space:]]|\n|\r|\t)+[!.]?[[:space:]]*$ ]]; then
+        trigger="yelling"
+
+    # Look for whitespace only.
+    elif [[ ${input} =~ ^([[:space:]]|\n|\r|\t)*$ ]]; then
+        trigger="catchall"
+
+    # Look for a statement.
+    elif [[ ${input} =~ ^([[:digit:]]|[[:alpha:]]|[[:punct:]]|[[:space:]]|\n|\r|\t)+[!.]?$ ]]; then
+        trigger="statement"
+
+    # Everything else.
+    else
+        trigger="catchall"
+    fi
+
+    response="${responses[${trigger}]}"
+
+    eprintf "\n"
+    eprintf "Input   : %s\n" "${input}"
+    eprintf "\n"
+    eprintf "Response: "
+    printf "%s\n" "${response}"
+}
+
+# Run the main function.
+main "${@}"
