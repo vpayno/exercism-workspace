07beb8148cd67f0886e0c4541fd30b34c32b9455
Victor Payno <vpayno@users.noreply.github.com>
2021-10-31 18:43:29 -0700

    bash/acronym: completed assignment

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -1,24 +1,80 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# Enable/Disable debug output.
+declare DEBUG="false"
+
+# Function: printf to stderr if DEBUG is set to true
+# Input : same as printf
+# Output: same as printf, except it goes to stderr instead of stdout
+# Return: from printf
+eprintf()
+{
+    # Pass all the printf arguments and redirect stdout to stderr.
+    # shellcheck disable=SC2059
+    ${DEBUG} && printf "$@" >&2
+}
+
+# Function: check the validity of the script's arguments.
+# Input : script arguments
+# Output: error messages
+# Return: 0 (valid) or >=1 (one or more input problems found)
+check_args()
+{
+    # Capture the script's arguments.
+    local -a vargs=( "$@" )
+
+    # Variable used to track the return codes from each of the checks.
+    local -i retval=0
+
+    # Was a single argument passed?
+    if [[ ${#vargs[@]} -ne 1 ]]; then
+        eprintf "ERROR: wrong number of arguments were passed [%s](%d)\n" "${vargs[*]}" "${#vargs[@]}"
+        show_usage
+        (( retval++ ))
+    fi
+
+    # If a non-zero value is returned, it means that one or more tests have failed.
+    return "${retval}"
+}
+
+# Function: print the scripts usage help message.
+# Input : none
+# Output: usage help screen
+# Return: 1 (always)
+show_usage()
+{
+    printf "Usage: %s \"<string>\"" "$0"
+}
+
+# Check the inputs for validity and exit if the checks fail.
+check_args "$@" || exit "$?"
+
+# Function: main function
+# Input : a multi-token string
+# Output: the acronym of the input
+# Return: 0 (success) or >=1 (error)
+main()
+{
+    # Convert underscores and dashes to spaces. It will make tokenizing the string easier.
+    # The order matters in the regex when you have special characters like dashes, asterisk, etc.
+    local input="${1//[*?_-]/ }"
+
+    local -a words
+    local word
+    local output=""
+
+    # Tokenize the string into an array. At the same time uppercase the tokens.
+    read -r -a words <<< "${input^^}"
+
+    # Walk the words array and grab the first letter from each word.
+    for word in "${words[@]}"; do
+        output+="${word:0:1}"
+    done
+
+    eprintf "\nInput: %s\n" "${input}"
+    eprintf "\nAcronym: "
+    printf "%s\n" "${output}"
+}
+
+# Run the main function.
+main "${@}"
