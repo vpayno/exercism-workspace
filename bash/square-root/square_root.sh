dcf1b11a5c62e26d9de88aafff3455a42f7b6192
Victor Payno <vpayno@users.noreply.github.com>
2021-12-26 23:08:50 -0800

    bash/square-root: 1st iteration

--- bash/square-root/square_root.sh
+++ bash/square-root/square_root.sh
@@ -1,24 +1,99 @@
 #!/usr/bin/env bash
+#
+# Bash Reference Manual (HTML)        : https://www.gnu.org/software/bash/manual/
+# Bash Reference Manual (PDF)         : https://www.gnu.org/s/bash/manual/bash.pdf
+#
+# Bash Guide for Beginners (HTML)     : https://tldp.org/LDP/Bash-Beginners-Guide/html/Bash-Beginners-Guide.html
+# Bash Guide for Beginners (PDF)      : https://tldp.org/LDP/Bash-Beginners-Guide/Bash-Beginners-Guide.pdf
+#
+# Advanced Bash-Scripting Guide (HTML): https://tldp.org/LDP/abs/html/
+# Advanced Bash-Scripting Guide (PDF) : https://tldp.org/LDP/abs/abs-guide.pdf
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# Enable/Disable debug output.
+# This syntax let's us run it like this: DEBUG=true bash ./script_name.sh parameters
+: "${DEBUG:=false}"
+
+# Protect oursevles from code injection.
+if [[ ! ${DEBUG,,} =~ ^(false|true)$ ]]; then
+    DEBUG="false"
+fi
+
+# Lowercase the string and add /bin/ to the start of the string.
+DEBUG="/bin/${DEBUG,,}"
+
+# Function: printf to stderr if DEBUG is set to true
+# Input : same as printf
+# Output: same as printf, except it goes to stderr instead of stdout
+# Return: from printf
+eprintf()
+{
+    # Pass all the printf arguments and redirect stdout to stderr.
+    # shellcheck disable=SC2059
+    ${DEBUG} && printf "$@" >&2
+} # eprintf()
+
+# Function: check the validity of the script's arguments.
+# Input : script arguments
+# Output: error messages
+# Return: 0 (valid) or >=1 (one or more input problems found)
+check_args()
+{
+    # Capture the script's arguments.
+    local -a vargs=( "$@" )
+    local number="${1}"
+
+    # Variable used to track the return codes from each of the checks.
+    local -i retval=0
+
+    # Two arguments?
+    if [[ ${#vargs[@]} -ne 1 ]]; then
+        eprintf "ERROR: wrong number of arguments were passed [%s](%d)\n" "${vargs[*]}" "${#vargs[@]}"
+        show_usage
+        (( retval++ ))
+
+    # First argument: a number?
+    elif [[ ! ${number} =~ ^[0-9]+$ ]]; then
+        eprintf "ERROR: argument [%s] can only be a number.\n" "${number}"
+        show_usage
+        (( retval++ ))
+    fi
+
+    # If a non-zero value is returned, it means that one or more tests have failed.
+    return "${retval}"
+} # check_args()
+
+# Function: print the scripts usage help message.
+# Input : none
+# Output: usage help screen
+# Return: 1 (always)
+show_usage()
+{
+    {
+        printf "%s\n\n" "Calculates the square root of a number."
+        printf "Usage: %s number" "$0"
+        printf "\n"
+    } >&2
+} # show_usage()
+
+# Check the inputs for validity and exit if the checks fail.
+check_args "$@" || exit "$?"
+
+# Function: main function
+# Input : script argument(s)
+# Output: exercise output
+# Return: 0
+main()
+{
+    local -i number="${1}"
+
+    # The tests only test perfect squares.
+    local -i result
+
+    result="$(bc <<< "scale=0; sqrt(${number})/1")"
+
+    eprintf "Square root of [%s] is [%s].\n" "${number}" "${result}"
+    ${DEBUG} || printf "%s\n" "${result}"
+} # main()
+
+# Run the main function.
+main "${@}"
