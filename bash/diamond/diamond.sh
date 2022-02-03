a20f596368c1cb36534b8a8cef3d2e7aef9197f7
Victor Payno <vpayno@users.noreply.github.com>
2022-02-02 21:43:10 -0800

    bash/diamond: 2nd iteration - in progress, optimizations

--- bash/diamond/diamond.sh
+++ bash/diamond/diamond.sh
@@ -89,63 +89,25 @@ generate_diamond()
     local -a lines
     local line
 
-    local -A letter2int=(
-        [A]=1
-        [B]=2
-        [C]=3
-        [D]=4
-        [E]=5
-        [F]=6
-        [G]=7
-        [H]=8
-        [I]=9
-        [J]=10
-        [K]=11
-        [L]=12
-        [M]=13
-        [N]=14
-        [O]=15
-        [P]=16
-        [Q]=17
-        [R]=18
-        [S]=19
-        [T]=20
-        [U]=21
-        [V]=22
-        [W]=23
-        [X]=24
-        [Y]=25
-        [Z]=26
-    )
+    local -A letter2int
+    local -A int2letter
+    local letters
+    local f
 
-    local -A int2letter=(
-		[1]=A
-		[2]=B
-		[3]=C
-		[4]=D
-		[5]=E
-		[6]=F
-		[7]=G
-		[8]=H
-		[9]=I
-		[10]=J
-		[11]=K
-		[12]=L
-		[13]=M
-		[14]=N
-		[15]=O
-		[16]=P
-		[17]=Q
-		[18]=R
-		[19]=S
-		[20]=T
-		[21]=U
-		[22]=V
-		[23]=W
-		[24]=X
-		[25]=Y
-		[26]=Z
-    )
+    # Generate a string with the letters A to Z.
+    # Adding an offset to the start of the string so the letters have indexes 1-26.
+    letters="_$(printf "%s" {A..Z})"
+
+    # Generate a key table so we can translate a letter to it's output value.
+    # f -> forward index
+    for (( f=1; "${f}" <= 26; f++ )); do
+        # Instead of manually writing these out, let's auto-generate the key table.
+        # shellcheck disable=SC2046
+        {
+            eval $(printf "letter2int[\"%s\"]=\"%d\"\n" "${letters:${f}:1}" "${f}")
+            eval $(printf "int2letter[\"%d\"]=\"%s\"\n" "${f}" "${letters:${f}:1}")
+        }
+    done
 
     local -i end_letter_index
     local -i end_of_line
