e2582a5f7daac13f606f5eebfcd7a38ec934f7a3
Victor Payno <vpayno@users.noreply.github.com>
2022-02-21 18:50:17 -0800

    bash/nth-prime: 1st iteration

--- bash/nth-prime/nth_prime.sh
+++ bash/nth-prime/nth_prime.sh
@@ -1,24 +1,320 @@
 #!/usr/bin/env bash
+#
+# Bash Reference Manual (HTML)        : https://www.gnu.org/software/bash/manual/
+# Bash Reference Manual (PDF)         : https://www.gnu.org/s/bash/manual/bash.pdf
+#
+# Bash Guide for Beginners (HTML)     : https://tldp.org/LDP/Bash-Beginners-Guide/html/Bash-Beginners-Guide.html
+# Bash Guide for Beginners (PDF)      : https://tldp.org/LDP/Bash-Beginners-Guide/Bash-Beginners-Guide.pdf
+#
+# Advanced Bash-Scripting Guide (HTML): https://tldp.org/LDP/abs/html/
+# Advanced Bash-Scripting Guide (PDF) : https://tldp.org/LDP/abs/abs-guide.pdf
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# Enable/Disable debug output.
+# This syntax let's us run it like this: DEBUG=true bash ./script_name.sh parameters
+: "${DEBUG:=false}"
+
+# Protect oursevles from code injection.
+if [[ ! ${DEBUG,,} =~ ^(false|true)$ ]]; then
+    DEBUG="false"
+fi
+
+# Lowercase the string and add /bin/ to the start of the string.
+DEBUG="/bin/${DEBUG,,}"
+
+# Function: printf to stderr if DEBUG is set to true
+# Input : same as printf
+# Output: same as printf, except it goes to stderr instead of stdout
+# Return: from printf
+eprintf()
+{
+    # Pass all the printf arguments and redirect stdout to stderr.
+    # shellcheck disable=SC2059
+    ${DEBUG} && printf "$@" >&2
+} # eprintf()
+
+# Function: check the validity of the script's arguments.
+# Input : script arguments
+# Output: error messages
+# Return: 0 (valid) or >=1 (one or more input problems found)
+check_args()
+{
+    # Capture the script's arguments.
+    local -a vargs=( "$@" )
+    local input="${1}"
+
+    # Variable used to track the return codes from each of the checks.
+    local -i retval=0
+
+    # One argument?
+    if [[ ${#vargs[@]} -ne 1 ]]; then
+        eprintf "ERROR: wrong number of arguments were passed [%s](%d)\n" "${vargs[*]}" "${#vargs[@]}"
+        #show_usage
+        (( retval++ ))
+
+    # First argument: A number?
+    elif [[ ! ${input} =~ ^[[:digit:]]+$ ]] || [[ ${input} -lt 1 ]]; then
+        eprintf "ERROR: can only be a positive input greater than or equals to 1 [%s].\n" "${input}"
+        printf "invalid input\n"
+        #show_usage
+        (( retval++ ))
+
+    fi
+
+    # If a non-zero value is returned, it means that one or more tests have failed.
+    return "${retval}"
+} # check_args()
+
+# Function: print the scripts usage help message.
+# Input : none
+# Output: usage help screen
+# Return: 1 (always)
+show_usage()
+{
+    {
+        printf "\n%s\n\n" "Given a number n, determine what the nth prime is."
+        printf "Usage: %s <number greater than or equals to 1>" "$0"
+        printf "\n"
+    } >&2
+} # show_usage()
+
+# Check the inputs for validity and exit if the checks fail.
+# Usually when you show the usage screen because the wrong arguments were passed,
+# it's an error, marking it not an error so the tests pass.
+# Also, we don't output anything for the same test.
+check_args "$@" || exit 1
+
+# Function: uses sieve of eratosthenes algorithm to find the prime numbers in a sequence
+# Input : number greather than or equals to 2
+# Output: a newline delimeted list of prime numbers
+# Return: 0
+find_primes_soe()
+{
+    local -i upper_limit="${1}"
+    local -i stop_limit="${2:-${upper_limit}}"
+    local -n __primes="${3}"
+
+    local -i index
+    local -A sequence
+    local -i inner
+    local -i outer
+    local -i step
+
+    # 5th optimization: only add odd numbers to the sequence
+    index="2"
+    sequence["${index}"]="unknown"
+    index="3"
+    step=2
+
+    # 4th optimization: don't use seq since it's very slow
+    while [[ ${index} -le ${upper_limit} ]]; do
+        sequence["${index}"]="unknown"
+        index+="${step}"
+        #(( index+=2 ))
+    done
+
+    # The keys aren't guaranteed to be sorted.
+    for outer in $(printf "%d\n" "${!sequence[@]}" | sort -n); do
+        # eprintf "outer=%d\n" "${outer}"
+
+        # 5th optimization: don't use seq since it's very slow
+        inner="${outer}"
+        step="${outer}"
+        while [[ ${inner} -le "${upper_limit}" ]]; do
+            # eprintf "\tinner=%d\n" "${inner}"
+
+            if [[ ${outer} == "${inner}" ]] && [[ ${sequence[${inner}]:-unknown} == "unknown" ]]; then
+                sequence["${inner}"]="prime"
+                __primes+=( "${inner}" )
+                # eprintf "\t\t%d => %s\n" "${inner}" "prime"
+
+            elif [[ ${sequence[${inner}]} == unknown ]]; then
+                sequence["${inner}"]="not_prime"
+                # eprintf "\t\t%d => %s\n" "${inner}" "not a prime"
+
+            fi
+
+            # 2nd optimization: stop searching when we reach the nth prime
+            if [[ ${#__primes[@]} -ge "${stop_limit}" ]]; then
+                eprintf "Found enough prime numbers [%d] >= [%d]\n" "${#__primes[@]}" "${stop_limit}"
+                break 2
+            fi
+
+            inner+="${step}"
+        done
+    done
+
+    eprintf "Prime numbers: "
+    eprintf "%s, " "${__primes[@]}"
+    eprintf "\n"
+
+    # Returned by refrence now.
+    # printf "%s\n" "${primes[@]}"
+
+    return 0
+} # find_primes_soe()
+
+# Function: uses sieve of atkin algorithm to find the prime numbers in a sequence
+# Input : number greater than or equals to 1
+# Output: a newline delimited list of prime numbers
+# Return: 0
+find_primes_soa()
+{
+    local -i upper_limit="${1}"
+    local -n __primes2="${2}"
+
+    local -i inner
+    local -i outer
+    local -i n
+    local -A -i sequence
+    local -i index
+    local -i step
+
+    # known prime
+    if [[ ${upper_limit} -gt 2 ]]; then
+        __primes2+=( 2 )
+    fi
+
+    # known prime
+    if [[ ${upper_limit} -gt 3 ]]; then
+        __primes2+=( 3 )
+    fi
+
+    # 0: true, 1: false
+    index=0
+    while [[ ${index} -lt ${upper_limit} ]]; do
+        sequence["${index}"]="1"  # false
+        index+="1"
+    done
+
+    outer="1"
+    while (( outer * outer < upper_limit )); do
+
+        inner="1"
+        while (( inner * inner < upper_limit )); do
+
+            n="(4 * outer * outer) + (inner * inner)"
+            eprintf "(4 * %d * %d) + (%d * %d) = %d\n" "${outer}" "${outer}" "${inner}" "${inner}" "${n}"
+
+            if (( (n <= upper_limit) && (n % 12 == 1 || n % 12 == 5) )); then
+                sequence["${n}"]="1 - ${sequence[${n}]}"  # toggle boolean at n
+            fi
+
+            n="(3 * outer * outer) + (inner * inner)"
+            eprintf "(3 * %d * %d) + (%d * %d) = %d\n" "${outer}" "${outer}" "${inner}" "${inner}" "${n}"
+
+            if (( (n <= upper_limit) && (n % 12 == 7) )); then
+                sequence["${n}"]="1 - ${sequence[${n}]}"  # toggle boolean at n
+            fi
+
+            n="(3 * outer * outer) - (inner * inner)"
+            eprintf "(3 * %d * %d) - (%d * %d) = %d\n" "${outer}" "${outer}" "${inner}" "${inner}" "${n}"
+
+            if (( (outer > inner) && (n <= upper_limit) && (n % 12 == 11) )); then
+                # toggle boolean at n
+                sequence["${n}"]="1 - ${sequence[${n}]}"
+            fi
+
+            inner+=1
+        done
+
+        outer+=1
+
+    done
+
+    # Mark all multiples of squares as non-primes.
+    outer=5
+    while (( outer * outer < upper_limit )); do
+
+        if [[ ${sequence[${outer}]} -eq 0 ]]; then
+
+            inner="${outer} * ${outer}"
+            step="${outer} * ${outer}"
+            while (( inner < upper_limit )); do
+                sequence["${inner}"]="1" # False
+
+                eprintf "sequence[%d]=1\n" "${inner}"
+
+                inner+="${step}"
+            done
+
+        fi
+
+        outer+="1"
+
+    done
+
+    index=0
+    while [[ ${index} -lt ${upper_limit} ]]; do
+        if [[ ${sequence[${index}]} -eq 0 ]]; then
+            __primes2+=( "${index}" )
+            eprintf "__primes2+=( \"%d\" )\n" "${index}"
+        fi
+    done
+
+    eprintf "Prime numbers (%d): " "${#__primes2[@]}"
+    eprintf "%s, " "${__primes2[@]}"
+    eprintf "\n"
+
+    return 0
+} # find_primes_soa()
+
+# Function: use either soe or soa algorithims to find a prime number based on the size of n.
+# Input : number greater than or equals to 1
+# Output: a newline delimited list of prime numbers
+# Return: 0
+find_primes()
+{
+    local -i n="${1}"
+    local -n __prime="${2}"
+
+    local -a -i primes
+    local -i search_size="${n}"
+    local -i search_loops=0
+
+    # 1st optimization: use a large search size to minimize restarts
+    (( search_size *= 20 ))
+
+    while [[ ${#primes[@]} -lt "${input}" ]]; do
+        (( search_loops++ ))
+
+        eprintf "Search loop [%d], # of primes [%d]\n\n" "${search_loops}" "${#primes[@]}"
+
+        eprintf "Using Sieve of Eratosthenes algorithim.\n"
+
+        # read -r -d "\n" -a primes < <(find_primes_soe "${search_size}" "${n}")
+        find_primes_soe "${search_size}" "${n}" "primes"
+
+        # 3rd optimization: only return the last element
+        # printf "%s\n" "${primes[@]: -1}"
+        __prime="${primes[*]: -1}"
+
+        (( search_size *= 10 ))
+    done
+
+    eprintf "Search loop [%d], # of primes [%d]\n\n" "${search_loops}" "${#primes[@]}"
+
+    return 0
+} # find_primes()
+
+# Function: main function
+# Input : script argument(s)
+# Output: exercise output
+# Return: 0
+main()
+{
+    local -i input="${1}"
+
+    local -i prime
+
+    # read -r -d "\n" -a primes < <(find_primes "${input}")
+    find_primes "${input}" "prime"
+
+    # Don't forget to trim the traling white space so the tests pass.
+    # printf "%s " "${primes[$(( input - 1 ))]}" | sed -r -e 's/ +$//g'
+    # printf "%s\n" "${primes[@]}"
+    printf "%s\n" "${prime}"
+    # printf "\n"
+} # main()
+
+# Run the main function.
+main "${@}"
