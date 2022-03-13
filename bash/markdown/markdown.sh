f6181a2c4108f04a188500ab2c1f6d0c9df1a1b8
Victor Payno <vpayno@users.noreply.github.com>
2022-03-13 09:32:45 -0700

    bash/markdown: 3rd iteration - style clean up

--- bash/markdown/markdown.sh
+++ bash/markdown/markdown.sh
@@ -2,120 +2,163 @@
 
 # 1. Clean up whitespace.
 # 2. Address all shellcheck (v0.8.0) issues.
+# 3. Style clean up.
 
 while IFS= read -r line; do
 
 	while true; do
 
-		orig=$line
+		# {} braces and double quotes
+		orig="${line}"
 
-		if [[ $line =~ ^(.+)__(.*) ]]; then
+		# {} around variables
+		if [[ ${line} =~ ^(.+)__(.*) ]]; then
 
-			post=${BASH_REMATCH[2]};pre=${BASH_REMATCH[1]}
+			# double quotes, breakup line
+			post="${BASH_REMATCH[2]}"
+			pre="${BASH_REMATCH[1]}"
 
-			if [[ $pre =~ ^(.*)__(.+) ]]; then
+			# {} around variables
+			if [[ ${pre} =~ ^(.*)__(.+) ]]; then
 
-				printf -v line "%s<strong>%s</strong>%s" "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" "$post"
+				# {} around variables
+				printf -v line "%s<strong>%s</strong>%s" "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" "${post}"
 
 			fi
 
 		fi
 
-		[ "$line" != "$orig" ] || break
+		# use [[ ]] instead of [ ], add {} to variables
+		[[ ${line} != "${orig}" ]] || break
 
 	done
 
 	# Check exit code directly with e.g. 'if mycmd;', not indirectly with $?. [SC2181]
-	if echo "$line" | grep '^\*' > /dev/null 2>&1; then
+	# use -q instead of the redirect to null for stdout
+	# add {} to line
+	if echo "${line}" | grep -q '^\*' 2> /dev/null; then
 
 
 		# Double quote to prevent globbing and word splitting. [SC2086]
-		if [ "X$inside_a_list" != Xyes ]; then
+		# use [[ ]] instead of [ ]
+		# use a default value instead of pre-pending an X
+		if [[ ${inside_a_list:-no} != yes ]]; then
 
-			h="$h<ul>"
-			inside_a_list=yes
+			# {} around variables
+			h="${h}<ul>"
+
+			# quote right-hand side of variable assignments
+			inside_a_list="yes"
 
 		fi
 
-		while [[ $line == *_*?_* ]]; do
+		# {} around variables
+		while [[ ${line} == *_*?_* ]]; do
 
-			one=${line#*_}
-			two=${one#*_}
+			# quote right-hand side of the variable assignment
+			one="${line#*_}"
+			two="${one#*_}"
 
 			# Double quote to prevent globbing and word splitting. [SC2086] - shellcheck misssed this one
 			# Prefer [ p ] && [ q ] as [ p -a q ] is not well defined. [SC2166]
-			if [ "${#two}" -lt "${#one}" ] && [ "${#one}" -lt "${#line}" ]; then
+			# use [[ ]] instead of [ ]
+			if [[ ${#two} -lt ${#one} ]] && [[ ${#one} -lt ${#line} ]]; then
 
 				# Expansions inside ${..} need to be quoted separately, otherwise they match as patterns. [SC2295]
-				line="${line%%_"$one"}<em>${one%%_"$two"}</em>$two"
+				line="${line%%_"${one}"}<em>${one%%_"${two}"}</em>${two}"
 
 			fi
 
 		done
 
-		h="$h<li>${line#??}</li>"
+		# {} around variables
+		h="${h}<li>${line#??}</li>"
 
 	else
 
 		# Double quote to prevent globbing and word splitting. [SC2086] - shellcheck missed this one
-		if [ "X$inside_a_list" = Xyes ]; then
+		# use [[ ]] instead of [ ]
+		# use a default value instead of pre-pending an X
+		# use == instead of =
+		if [[ ${inside_a_list:-no} == yes ]]; then
 
-			h="$h</ul>"
-			inside_a_list=no
+			# {} around variables
+			h="${h}</ul>"
+
+			# quote right-hand side of the variable assignment
+			inside_a_list="no"
 
 		fi
 
 		# Use $(...) notation instead of legacy backticks `...`. [SC2006]
-		n="$(expr "$line" : "#\{1,\}")"
+		# {} around variables
+		n="$(expr "${line}" : "#\{1,\}")"
 
 		# Double quote to prevent globbing and word splitting. [SC2086]
 		# Prefer [ p ] && [ q ] as [ p -a q ] is not well defined. [SC2166]
-		if [ "$n" -gt 0 ] && [ 7 -gt "$n" ]; then
+		# {} around variables
+		# use [[ ]] instead of [ ]
+		if [[ ${n} -gt 0 ]] && [[ 7 -gt ${n} ]]; then
 
-			while [[ $line == *_*?_* ]]; do
+			# {} around variables
+			while [[ ${line} == *_*?_* ]]; do
 
-				s=${line#*_}
-				t=${s#*_}
+				# quote right-hand side of variable assignments
+				s="${line#*_}"
+				t="${s#*_}"
 
 				# Double quote to prevent globbing and word splitting. [SC2086] - shellcheck misssed this one
 				# Prefer [ p ] && [ q ] as [ p -a q ] is not well defined. [SC2166]
-				if [ "${#t}" -lt "${#s}" ] && [ "${#s}" -lt "${#line}" ]; then
+				# use [[ ]] instead of [
+				if [[ ${#t} -lt ${#s} ]] && [[ ${#s} -lt ${#line} ]]; then
 
 					# Expansions inside ${..} need to be quoted separately, otherwise they match as patterns. [SC2295]
-					line="${line%%_"$s"}<em>${s%%_"$t"}</em>$t"
+					# {} around variables
+					line="${line%%_"${s}"}<em>${s%%_"${t}"}</em>${t}"
 
 				fi
 
 			done
 
-			HEAD=${line:n}
+			# quote right-hand side of the variable
+			HEAD="${line:n}"
 
-			while [[ $HEAD == " "* ]]; do
+			while [[ ${HEAD} == " "* ]]; do
 
-				HEAD=${HEAD# }
+				# quote right-hand side of the variable assignment
+				HEAD="${HEAD# }"
 
 			done
 
-			h="$h<h$n>$HEAD</h$n>"
+			# {} around variables
+			h="${h}<h${n}>${HEAD}</h${n}>"
 
 		else
 
 			# Use $(...) notation instead of legacy backticks `...`. [SC2006]
-			grep '_..*_' <<<"$line" > /dev/null && line="$(echo "$line" | sed -E 's,_([^_]+)_,<em>\1</em>,g')"
+			# {} around variables
+			# use -q instead of redirect of stdout
+			grep -q '_..*_' <<< "${line}" && line="$(echo "${line}" | sed -E 's,_([^_]+)_,<em>\1</em>,g')"
 
-			h="$h<p>$line</p>"
+			# {} around variables
+			h="${h}<p>${line}</p>"
 
 		fi
 
 	fi
 
-done < "$1"
+done < "${1}"  # {} around variables
 
 # Double quote to prevent globbing and word splitting. [SC2086] - shellcheck misssed this one
-if [ "X$inside_a_list" = Xyes ]; then
+# use [[ ]] instead of [ ]
+# use a default value instead of pre-pending an X
+# use == instead of =
+if [[ ${inside_a_list:-no} == yes ]]; then
 
-	h="$h</ul>"
+	# {} around variables
+	h="${h}</ul>"
 
 fi
 
-echo "$h"
+# {} around variables
+echo "${h}"
