78511fb266a4ac89b1293c9be5c5ba31834a279f
Victor Payno <vpayno@users.noreply.github.com>
2022-06-16 08:38:36 -0700

    go/weather-forcast: 1st iteration

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather is a library for forcasting weather conditions in various cities in Goblinocus.
 package weather
 
+// CurrentCondition stores the current weather conditions for a city.
 var CurrentCondition string
+
+// CurrentLocation stores the current city we're forcasting for.
 var CurrentLocation string
 
+// Forecast returns a string with the weather conditions for a city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
