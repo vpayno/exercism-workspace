e93940b3b3b2946bb3b574bad9d1f50bae2e3422
Victor Payno <vpayno@users.noreply.github.com>
2022-06-20 22:20:54 -0700

    go/annalyns-infiltration: 2nd iteration - submit examples file and practice go quick return if blocks

--- go/annalyns-infiltration/annalyns_infiltration.go
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -18,17 +18,13 @@ func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
 // or if Annalyn's pet dog is with her and the archer is sleeping
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-	var result bool
-
 	if !prisonerIsAwake && !petDogIsPresent {
-		result = false
-	} else {
-		if petDogIsPresent {
-			result = !archerIsAwake
-		} else {
-			result = !knightIsAwake && !archerIsAwake
-		}
+		return false
 	}
 
-	return result
+	if petDogIsPresent {
+		return !archerIsAwake
+	}
+
+	return !knightIsAwake && !archerIsAwake
 }
