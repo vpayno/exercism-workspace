5a2b986d5a3ff48b0b9fcc6500c1738c2b6e8663
Victor Payno <vpayno@users.noreply.github.com>
2022-06-22 23:31:15 -0700

    go/isogram: 4th iteration

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -9,15 +9,21 @@ import (
 // IsIsogram returns true when a word is an isogram.
 // An isogram (also known as a "non-pattern word") is a word or phrase without a repeating letter, however spaces and hyphens are allowed to appear multiple times.
 func IsIsogram(word string) bool {
-	word = strings.ToLower(word)
-
 	r, e := regexp.Compile(`[^a-z]`)
 
 	if e != nil {
 		panic(e)
 	}
 
-	word = r.ReplaceAllString(word, "")
+	repl := func(match string) string {
+		if match == "-" || match == " " {
+			return ""
+		}
+
+		return strings.ToLower(match)
+	}
+
+	word = r.ReplaceAllStringFunc(word, repl)
 
 	for i1, l1 := range word {
 		for i2, l2 := range word {
