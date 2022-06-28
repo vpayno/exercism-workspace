30535e91fa3e9dc5b8476a79c1d4f703bd370ae3
Victor Payno <vpayno@users.noreply.github.com>
2022-06-27 19:48:50 -0700

    go/nucleotide-count: 2nd iteration

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -2,7 +2,6 @@
 package dna
 
 import (
-	"errors"
 	"fmt"
 	"regexp"
 )
@@ -17,8 +16,23 @@ type Histogram map[rune]int
 // DNA is a list of nucleotides.
 type DNA string
 
-// Valid nucleotides.
-const nucleotides = "ACGT"
+// IsValid returns true if the DNA strand is valid.
+func (d DNA) IsValid() (bool, error) {
+	// Valid nucleotides.
+	const nucleotides = "ACGT"
+
+	if match, _ := regexp.MatchString(`^[`+nucleotides+`]+$`, string(d)); !match {
+		return false, fmt.Errorf("DNA sequence, %q, contains invalid nucleotides", string(d))
+	}
+
+	return true, nil
+}
+
+// String returns a pretty formated representation of a DNA type.
+func (d DNA) String() string {
+	// we need string(d) here to prevent recursion
+	return fmt.Sprintf("dna strand: %q", string(d))
+}
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
@@ -38,8 +52,8 @@ func (d DNA) Counts() (Histogram, error) {
 		return h, nil
 	}
 
-	if match, _ := regexp.MatchString(`^[`+nucleotides+`]+$`, string(d)); !match {
-		return h, errors.New("DNA sequence contains invalid nucleotides")
+	if ok, e := d.IsValid(); !ok {
+		return h, e
 	}
 
 	for _, nucleotide := range d {
@@ -47,7 +61,7 @@ func (d DNA) Counts() (Histogram, error) {
 	}
 
 	if debug {
-		fmt.Printf("dna: %q\n", d)
+		fmt.Printf("%s\n", d)
 		fmt.Printf("historgram: %#v\n", h)
 	}
 
