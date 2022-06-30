fd7564cbf13bc8bb4679c3af41a26b1138ccb8ae
Victor Payno <vpayno@users.noreply.github.com>
2022-06-30 07:00:34 -0700

    go/armstrong-numbers: 1st iteration

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,32 @@
+// Package armstrong checks that a number is an Armstrong number.
 package armstrong
 
-func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+// IsNumber returns true if the passed number equals the sum of it's digits to
+// the power of the number of digits.
+func IsNumber(number int) bool {
+	var digits string = fmt.Sprintf("%d", number)
+	var exponent float64 = float64(len(digits))
+	var sum int
+
+	for _, r := range digits {
+		n, e := strconv.Atoi(string(r))
+		if e != nil {
+			panic(e)
+		}
+
+		f := float64(n)
+		sum += int(math.Pow(f, exponent))
+	}
+
+	if number == sum {
+		return true
+	}
+
+	return false
 }
