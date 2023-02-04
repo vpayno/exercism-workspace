0f4be7daeeb4d26edc19bac61963e3476fb98ce4
Victor Payno <vpayno@users.noreply.github.com>
2023-02-04 13:58:15 -0800

    rust/lucians-luscious-lasagna: 1st iteration

--- rust/lucians-luscious-lasagna/src/lib.rs
+++ rust/lucians-luscious-lasagna/src/lib.rs
@@ -3,27 +3,41 @@
 #![allow(unused)]
 
 pub fn expected_minutes_in_oven() -> i32 {
-    unimplemented!("return expected minutes in the oven")
+    // Lasagnas always take 40 minutes regardless of the number of layers?
+    let minutes: i32 = 40;
+
+    // clippy will complain if we make the return explicit so just use
+    // a variable without a traling semicolon.
+    minutes
 }
 
 pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
-    unimplemented!(
-        "calculate remaining minutes in oven given actual minutes in oven: {}",
-        actual_minutes_in_oven
-    )
+    // Remaining time in the oven.
+    let remaining_time: i32 = expected_minutes_in_oven() - actual_minutes_in_oven;
+
+    // clippy will complain if we make the return explicit so just use
+    // a variable without a traling semicolon.
+    remaining_time
 }
 
 pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
-    unimplemented!(
-        "calculate preparation time in minutes for number of layers: {}",
-        number_of_layers
-    )
+    let minutes_per_layer: i32 = 2;
+
+    // Preparation time is dependent on the number of layers.
+    let preparation_time: i32 = number_of_layers * minutes_per_layer;
+
+    // clippy will complain if we make the return explicit so just use
+    // a variable without a traling semicolon.
+    preparation_time
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
-    unimplemented!(
-        "calculate elapsed time in minutes for number of layers {} and actual minutes in oven {}",
-        number_of_layers,
-        actual_minutes_in_oven
-    )
+    // Time spent preparing and cooking in the oven.
+    let mut time_elapsed: i32 = 0;
+    time_elapsed += preparation_time_in_minutes(number_of_layers);
+    time_elapsed += actual_minutes_in_oven;
+
+    // clippy will complain if we make the return explicit so just use
+    // a variable without a traling semicolon.
+    time_elapsed
 }
