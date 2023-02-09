da89dfd86b1d89ff180cb610aeb1bc6f4565e284
Victor Payno <vpayno@users.noreply.github.com>
2023-02-08 22:13:59 -0800

    dart/raindrops: 1st iteration

--- dart/raindrops/lib/raindrops.dart
+++ dart/raindrops/lib/raindrops.dart
@@ -1,3 +1,27 @@
+// Convert a number into a string that contains raindrop sounds corresponding to certain potential factors.
+// https://exercism.org/tracks/dart/exercises/raindrops
+
 class Raindrops {
-  // Put your code here
+  // convert(int) returns a string representation of the number
+  String convert(int number) {
+    var sounds = new StringBuffer();
+
+    if (number % 3 == 0) {
+      sounds.write("Pling");
+    }
+
+    if (number % 5 == 0) {
+      sounds.write("Plang");
+    }
+
+    if (number % 7 == 0) {
+      sounds.write("Plong");
+    }
+
+    if (sounds.length == 0) {
+      sounds.write(number.toString());
+    }
+
+    return sounds.toString();
+  }
 }
