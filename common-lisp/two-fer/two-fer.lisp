abe10287433e23423a230a09aa9376fc2a26eb8e
Victor Payno <vpayno@users.noreply.github.com>
2023-06-15 23:01:04 -0700

    common-lisp/two-fer: 2nd iteration

--- common-lisp/two-fer/two-fer.lisp
+++ common-lisp/two-fer/two-fer.lisp
@@ -3,7 +3,8 @@
   (:export :twofer))
 (in-package :two-fer)
 
-;;;
+;;; returns the string with the name as is or "you" if nil or absent
 (defun twofer (&optional (name))
+  ;; https://gigamonkeys.com/book/a-few-format-recipes.html#:~:text=With%20a%20colon%20modifier%2C%20the%20~%5B,directive%20must%20contain%20a%20~%3B.
   (format nil "One for ~:[you~;~A~], one for me." name name)
   )
