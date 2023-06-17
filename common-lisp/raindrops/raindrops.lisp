e26f6aaafe925b444c5d17dc5894c1bf023775b0
Victor Payno <vpayno@users.noreply.github.com>
2023-06-16 21:59:46 -0700

    common-lisp/raindrops: 3rd iteration - add 2 more functions for readability

--- common-lisp/raindrops/raindrops.lisp
+++ common-lisp/raindrops/raindrops.lisp
@@ -12,7 +12,7 @@
       (format nil "Pling") ; if true
     (format nil "")        ; else return an empty string
     ) ; endif
-  )
+  ) ; => "" or "Pling"
 
 ;;; returns Plang if evenly divisible by 5
 (defun plang (n)
@@ -22,7 +22,7 @@
       (format nil "Plang") ; if true
     (format nil "")        ; else return an empty string
     ) ; endif
-  )
+  ) ; => "" or "Plang"
 
 ;;; returns Plong if evenly divisible by 7
 (defun plong (n)
@@ -32,23 +32,32 @@
       (format nil "Plong") ; if true
     (format nil "")        ; else return an empty string
     ) ; endif
-  )
+  ) ; => "" or "Plong"
+
+;;; returns the number instead of the string if the string is empty.
+(defun sounds-or-number (str n)
+  (if (= 0 (length str))
+      (format nil "~A" n) ; if string is empty
+    (format nil "~A" str) ; else string has rain sounds
+    ) ; endif
+  ) ; => n or str
+
+;;; returns rainsounds from a number
+(defun rainsounds (n)
+  (concatenate 'string
+               (pling n)
+               (plang n)
+               (plong n)
+               )
+  ) ; => str
 
 ;;; returns the rain drop sounds for a given number
 (defun convert (n)
   "Converts a number to a string of raindrop sounds."
 
   (let ((str "")) ; create a local scope variable to the let expression
-    (setf str (concatenate 'string
-			   (pling n)
-			   (plang n)
-			   (plong n)
-			   ) ; set str to the format expression which concatenates 3 strings
-	  ) ; setf str
+    (setf str (rainsounds n)) ; set str to the rainsounds string
 
-    (if (= 0 (length str))
-        (format nil "~A" n) ; if string is empty
-      (format nil "~A" str) ; else string has rain sounds
-      ) ; endif
+    (sounds-or-number str n)
     ) ; let str
-  )
+  ) ; => n or str
