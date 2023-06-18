49fdf3606670d91e80ca396f86cfc565a530e045
Victor Payno <vpayno@users.noreply.github.com>
2023-06-18 12:20:56 -0700

    common-lisp/space-age: download exercise

---
+++ common-lisp/space-age/space-age.lisp
@@ -0,0 +1,12 @@
+(defpackage :space-age
+  (:use :cl)
+  (:export :on-mercury
+           :on-venus
+           :on-earth
+           :on-mars
+           :on-jupiter
+           :on-saturn
+           :on-uranus
+           :on-neptune))
+
+(in-package :space-age)
