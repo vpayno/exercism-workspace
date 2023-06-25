55cef11f57e5f09b02eac2ac3863dac2e84c3d78
Victor Payno <vpayno@users.noreply.github.com>
2023-06-24 22:40:50 -0700

    common-lisp/matching-brackets: 1st iteration - passing

--- common-lisp/matching-brackets/matching-brackets.lisp
+++ common-lisp/matching-brackets/matching-brackets.lisp
@@ -9,14 +9,17 @@
 ;;;; push open brackets into list
 ;;;; when closing bracket found, see if it matches last open bracket
 
+;;; set to t to enable debugging; to nil to disable debugging
+(defparameter debug-mode nil)
+
 ;;; test to see if closing bracket matches open bracket
 (defun bracket-match (open-bracket close-bracket)
   (cond
-    ((and (char= #\[ open-bracket) (char= #\] close-bracket)) t)
-    ((and (char= #\( open-bracket) (char= #\) close-bracket)) t)
-    ((and (char= #\{ open-bracket) (char= #\} close-bracket)) t)
-    (t nil)
-    )
+   ((and (char= #\[ open-bracket) (char= #\] close-bracket)) t)
+   ((and (char= #\( open-bracket) (char= #\) close-bracket)) t)
+   ((and (char= #\{ open-bracket) (char= #\} close-bracket)) t)
+   (t nil)
+   )
   ) ; => t or nil
 
 ;;; test to see if character is an opening bracket
@@ -45,10 +48,15 @@
 
 ;;; pops the last value on the open bracket stack and also return the new stack
 (defun pop-last (a)
-  (let ((result nil))
-    (values (setq result (last a)) (setq a (butlast a)))
+  (let ((popped nil))
+    (values (setq popped (last a)) (setq a (butlast a)))
     )
   ) ; => (G) ; (A B C D E F)
+
+;;; remove the last value on the open bracket stack and also return the new stack
+(defun remove-last (a)
+  (setq a (butlast a))
+  ) ; => (A B C D E F)
 
 ;;; let's you peek at the tail of the open bracket stack to see if it's a match
 ;;; for the closing bracket you just found
@@ -56,26 +64,58 @@
   (first (last a))
   ) ; => G or nil
 
+;;; only print debug messages when degugging
+(defun debug-format (message)
+  (if debug-mode
+      (format t message)
+    )
+  ) ; nil
+
 ;;; look for matching bracket pairs
 (defun pairedp (value)
+  ;; fast return on corner cases
+  (when (= 0 (length value))
+    (return-from pairedp t)
+    )
+
   ;; using a list as a stack
-  (let ((bracket-stack ()))
+  (let ((bracket-stack ()) (result t))
     (loop for c across value do
           (when (bracket-either c)
+            (debug-format (format nil "~A" c))
             (if (bracket-open c)
-              (setq bracket-stack (push-last c bracket-stack))
+		(setq bracket-stack (push-last c bracket-stack))
               (if (and (bracket-close c) (= 0 (length bracket-stack)))
-                      (return nil)
-                      (if (bracket-match (peek bracket-stack) c)
-                        (multiple-value-bind
-                          (last-open bracket-stack)
-                          (pop-last bracket-stack)
-                          ) ; multiple-value-bind
-                        (return nil)
-                      ) ; if closing matches open; else unmatched
+                  (progn
+                    (setq result nil)
+                    (debug-format (format nil "return from close '~A' before open '~A'" c bracket-stack))
+                    (return-from pairedp result)
+                    )
+                (if (bracket-match (peek bracket-stack) c)
+                    (progn
+                      (setq bracket-stack (remove-last bracket-stack))
+                      (debug-format (format nil "matched, removing last from stack: '~A'~%" bracket-stack))
+                      )
+                  (progn
+                    (setq result nil)
+                    (debug-format (format nil "return from close '~A' doesn't match open '~A' stack:'~A'~%" c (peek bracket-stack) bracket-stack))
+                    (return-from pairedp result)
+                    )
+                  ) ; if closing matches open; else unmatched
                 ) ; if open and stack empty; else open and stack not empty
               ) ; if open; else close
             ) ; when either
-      ) ; loop
+	  ) ; loop
+    (debug-format (format nil "~%"))
+    (if (= 0 (length bracket-stack))
+        (progn
+          (debug-format (format nil "return from bracket-stack is empty~%"))
+          (return-from pairedp t)
+          )
+      (progn
+        (debug-format (format nil "return from bracket-stack '~A' is not empty~%" bracket-stack))
+        (return-from pairedp nil)
+        )
+      )
     ) ; let
   ) ; => t or nil
