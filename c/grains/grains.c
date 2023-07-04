bafd9d83d2504ea81d0314e8dd90df073ecef2f2
Victor Payno <vpayno@users.noreply.github.com>
2023-07-03 20:31:34 -0700

    c/grains: 1st iteration

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,15 @@
 #include "grains.h"
+#include <math.h>
+
+uint64_t square(uint8_t index) {
+    const uint8_t index_min = 1;
+    const uint8_t index_max = 64;
+
+    if (index < index_min || index > index_max) {
+        return (uint64_t)0;
+    }
+
+    return (uint64_t)pow(2, index - 1);
+}
+
+uint64_t total() { return (uint64_t)pow(2, 64); }
