ec448fc5188dcc35504aef360c45bd3ae9b72689
Victor Payno <vpayno@users.noreply.github.com>
2023-07-17 12:36:57 -0700

    c++/vehicle-purchase: lint clean up

--- cpp/vehicle-purchase/vehicle_purchase.cpp
+++ cpp/vehicle-purchase/vehicle_purchase.cpp
@@ -4,13 +4,14 @@ namespace vehicle_purchase {
 
 // needs_license determines whether a license is needed to drive a type of
 // vehicle. Only "car" and "truck" require a license.
-bool needs_license(std::string kind) {
+bool needs_license(const std::string kind) {
     return kind == "truck" || kind == "car";
 }
 
 // choose_vehicle recommends a vehicle for selection. It always recommends the
 // vehicle that comes first in lexicographical order.
-std::string choose_vehicle(std::string option1, std::string option2) {
+std::string choose_vehicle(const std::string option1,
+                           const std::string option2) {
     std::string choice;
     const std::string comment = " is clearly the better choice.";
 
@@ -25,7 +26,8 @@ std::string choose_vehicle(std::string option1, std::string option2) {
 
 // calculate_resell_price calculates how much a vehicle can resell for at a
 // certain age.
-double calculate_resell_price(double original_price, double age) {
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+double calculate_resell_price(const double original_price, const double age) {
     double offset{0.50};
 
     if (age < 3) {
--- cpp/vehicle-purchase/vehicle_purchase_test.cpp
+++ cpp/vehicle-purchase/vehicle_purchase_test.cpp
@@ -7,99 +7,99 @@
 #endif
 
 TEST_CASE("need a license for a car") {
-    std::string kind{"car"};
+    const std::string kind{"car"};
     REQUIRE(vehicle_purchase::needs_license(kind));
 }
 
 #if defined(EXERCISM_RUN_ALL_TESTS)
 
 TEST_CASE("need a license for a truck") {
-    std::string kind{"truck"};
+    const std::string kind{"truck"};
     REQUIRE(vehicle_purchase::needs_license(kind));
 }
 TEST_CASE("does not need a license for a bike") {
-    std::string kind{"bike"};
+    const std::string kind{"bike"};
     REQUIRE_FALSE(vehicle_purchase::needs_license(kind));
 }
 TEST_CASE("does not need a license for a stroller") {
-    std::string kind{"stroller"};
+    const std::string kind{"stroller"};
     REQUIRE_FALSE(vehicle_purchase::needs_license(kind));
 }
 TEST_CASE("does not need a license for a e-scooter") {
-    std::string kind{"e-scooter"};
+    const std::string kind{"e-scooter"};
     REQUIRE_FALSE(vehicle_purchase::needs_license(kind));
 }
 
 TEST_CASE("chooses Bugatti over Ford") {
-    std::string choice1{"Bugatti Veyron"};
-    std::string choice2{"Ford Pinto"};
+    const std::string choice1{"Bugatti Veyron"};
+    const std::string choice2{"Ford Pinto"};
     REQUIRE(vehicle_purchase::choose_vehicle(choice1, choice2) ==
             "Bugatti Veyron is clearly the better choice.");
 }
 TEST_CASE("chooses Chery over Kia") {
-    std::string choice1{"Chery EQ"};
-    std::string choice2{"Kia Niro Elektro"};
+    const std::string choice1{"Chery EQ"};
+    const std::string choice2{"Kia Niro Elektro"};
     REQUIRE(vehicle_purchase::choose_vehicle(choice1, choice2) ==
             "Chery EQ is clearly the better choice.");
 }
 TEST_CASE("chooses Ford Focus over Ford Pinto") {
-    std::string choice1{"Ford Focus"};
-    std::string choice2{"Ford Pinto"};
+    const std::string choice1{"Ford Focus"};
+    const std::string choice2{"Ford Pinto"};
     REQUIRE(vehicle_purchase::choose_vehicle(choice1, choice2) ==
             "Ford Focus is clearly the better choice.");
 }
 TEST_CASE("chooses 2018 over 2020") {
-    std::string choice1{"2018 Bergamont City"};
-    std::string choice2{"2020 Gazelle Medeo"};
+    const std::string choice1{"2018 Bergamont City"};
+    const std::string choice2{"2020 Gazelle Medeo"};
     REQUIRE(vehicle_purchase::choose_vehicle(choice1, choice2) ==
             "2018 Bergamont City is clearly the better choice.");
 }
 TEST_CASE("chooses Bugatti over ford") {
-    std::string choice1{"Bugatti Veyron"};
-    std::string choice2{"ford"};
+    const std::string choice1{"Bugatti Veyron"};
+    const std::string choice2{"ford"};
     REQUIRE(vehicle_purchase::choose_vehicle(choice1, choice2) ==
             "Bugatti Veyron is clearly the better choice.");
 }
 
 TEST_CASE("price is reduced to 80% for age of 2") {
-    double original_price{40000};
-    double age{2};
-    double expected{32000};
+    const double original_price{40000};
+    const double age{2};
+    const double expected{32000};
     REQUIRE(vehicle_purchase::calculate_resell_price(original_price, age) ==
             expected);
 }
 TEST_CASE("price is reduced to 80% for age of 2.5") {
-    double original_price{40000};
-    double age{2.5};
-    double expected{32000};
+    const double original_price{40000};
+    const double age{2.5};
+    const double expected{32000};
     REQUIRE(vehicle_purchase::calculate_resell_price(original_price, age) ==
             expected);
 }
 TEST_CASE("price is reduced to 70% for age 7") {
-    double original_price{40000};
-    double age{7};
-    double expected{28000};
+    const double original_price{40000};
+    const double age{7};
+    const double expected{28000};
     REQUIRE(vehicle_purchase::calculate_resell_price(original_price, age) ==
             expected);
 }
 TEST_CASE("price is reduced to 50% for age 10") {
-    double original_price{25000};
-    double age{10};
-    double expected{12500};
+    const double original_price{25000};
+    const double age{10};
+    const double expected{12500};
     REQUIRE(vehicle_purchase::calculate_resell_price(original_price, age) ==
             expected);
 }
 TEST_CASE("price is reduced to 50% for age 11") {
-    double original_price{50000};
-    double age{11};
-    double expected{25000};
+    const double original_price{50000};
+    const double age{11};
+    const double expected{25000};
     REQUIRE(vehicle_purchase::calculate_resell_price(original_price, age) ==
             expected);
 }
 TEST_CASE("float price is reduced to 70% for age 8,") {
-    double original_price{39000.000001};
-    double age{8};
-    double expected{27300.0000007};
+    const double original_price{39000.000001};
+    const double age{8};
+    const double expected{27300.0000007};
     REQUIRE_THAT(vehicle_purchase::calculate_resell_price(original_price, age),
                  Catch::Matchers::WithinRel(expected, 0.001));
 }
