178d4d4b5eaccc81e01a65a5a6b8f7b1355874df
Victor Payno <vpayno@users.noreply.github.com>
2023-07-22 12:55:17 -0700

    c++/sieve: 3rd iteration - fixes after implementing nth-prime

--- cpp/sieve/sieve.cpp
+++ cpp/sieve/sieve.cpp
@@ -6,25 +6,38 @@ std::vector<int> primes(int limit) {
     std::vector<int> result{};
     std::vector<bool> numbers{};
 
+    // odd looking fast-return block
+    switch (limit) {
+    case 0:
+        [[fallthrough]];
+    case 1:
+        return result;
+    case 2:
+        result.push_back(2);
+        return result;
+    }
+
     numbers.resize(limit + 1, true);
     int index = 0;
     for (auto iter = numbers.begin(); iter < numbers.end(); iter++) {
         // if even -> (except for 2) can't be prime
-        if (index % 2 == 0) {
-            numbers[index] = false;
+        if (index == 2) {
+            numbers.at(2) = true;
+        } else if (index % 2 == 0) {
+            numbers.at(index) = false;
         }
 
         index += 1;
     }
-    numbers[0] = numbers[1] = false;
-    numbers[2] = true;
+    numbers.at(0) = numbers.at(1) = false;
+    numbers.at(2) = true;
 
     for (int number = 2; number <= limit; number++) {
         // is prime
-        if (numbers[number]) {
-            for (int i = 1, j = number * number; j <= limit;
-                 i++, j = number * number + i * number) {
-                numbers[j] = false;
+        if (numbers.at(number)) {
+            for (int j = 2; number * j <= limit; j++) {
+                const size_t index = size_t(number) * size_t(j);
+                numbers.at(index) = false;
             }
         }
     }
