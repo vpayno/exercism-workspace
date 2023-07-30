837c0c180963f6404a13f462e1e130d1cf4c6251
Victor Payno <vpayno@users.noreply.github.com>
2023-07-30 13:14:37 -0700

    c++/election-day: 2nd iteration - named types

--- cpp/election-day/election_day.cpp
+++ cpp/election-day/election_day.cpp
@@ -4,26 +4,30 @@
 
 namespace election {
 
-// The election result struct is already created for you:
+using CadidateName = std::string;
+using VoteCount = int;
 
+// The election result struct is already created for you:
 struct ElectionResult {
     // Name of the candidate
-    std::string name{};
+    CadidateName name{};
 
     // Number of votes the candidate has
-    int votes{};
+    VoteCount votes{};
 };
+
+using ElectionSummary = std::vector<ElectionResult>;
 
 // Task 1: vote_count()
 // - takes a reference to an `ElectionResult` as an argument
 // - will return the number of votes in the `ElectionResult`
-int vote_count(ElectionResult &result) { return result.votes; }
+VoteCount vote_count(ElectionResult &result) { return result.votes; }
 
 // Task 2: increment_vote_count()
 // - takes a reference to an `ElectionResult` as an argument
-// - and a number of votes (int)
+// - and a number of votes (VoteCount)
 // - increments the `ElectionResult` by that number of votes
-void increment_vote_count(ElectionResult &result, int count) {
+void increment_vote_count(ElectionResult &result, VoteCount count) {
     result.votes += count;
 }
 
@@ -32,12 +36,12 @@ void increment_vote_count(ElectionResult &result, int count) {
 // - returns a reference to the `ElectionResult` of the new president
 // - it also changes the name of the winner by prefixing it with "President"
 // - the final count is given in the form of a `reference` to
-// `std::vector<ElectionResult>`, a vector with `ElectionResults` of all the
+// `ElectionSummary`, a vector with `ElectionResults` of all the
 // participating candidates
-ElectionResult &determine_result(std::vector<ElectionResult> &final_count) {
+ElectionResult &determine_result(ElectionSummary &final_count) {
     int index{0};
     int counter{0};
-    int highest_score{0};
+    VoteCount highest_score{0};
 
     // index is a reference so we can access the "loop" value after
     // std::transform runs
