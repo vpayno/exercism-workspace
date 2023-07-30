760b9d3a41a9a4f48abe52a60dbea47ae1601b18
Victor Payno <vpayno@users.noreply.github.com>
2023-07-30 14:04:45 -0700

    c++/lasagna:: 3rd iteration - named types

--- cpp/lasagna/lasagna.cpp
+++ cpp/lasagna/lasagna.cpp
@@ -3,31 +3,32 @@ enum {
     TIME_PER_LAYER = 2,
 };
 
+using minutes_t = int;
+
 // ovenTime returns the amount in minutes that the lasagna should stay in the
 // oven.
-int ovenTime() { return OVEN_TIME; }
+minutes_t ovenTime() { return OVEN_TIME; }
 
 /* remainingOvenTime returns the remaining
    minutes based on the actual minutes already in the oven.
 */
-int remainingOvenTime(int actualMinutesInOven) {
-    // TODO: Calculate and return the remaining in the oven based on the time
-    // the lasagna has already been there.
+minutes_t remainingOvenTime(minutes_t actualMinutesInOven) {
+    // Calculate and return the remaining in the oven based on the time the
+    // lasagna has already been there.
     return ovenTime() - actualMinutesInOven;
 }
 
 /* preparationTime returns an estimate of the preparation time based on the
    number of layers and the necessary time per layer.
 */
-int preparationTime(int numberOfLayers) {
-    // TODO: Calculate and return the preparation time with the
-    // `numberOfLayers`.
+minutes_t preparationTime(minutes_t numberOfLayers) {
+    // Calculate and return the preparation time with the `numberOfLayers`.
     return TIME_PER_LAYER * numberOfLayers;
 }
 
 // elapsedTime calculates the total time spent to create and bake the lasagna so
 // far.
-int elapsedTime(int numberOfLayers, int actualMinutesInOven) {
-    // TODO: Calculate and return the total time so far.
+minutes_t elapsedTime(minutes_t numberOfLayers, minutes_t actualMinutesInOven) {
+    // Calculate and return the total time so far.
     return preparationTime(numberOfLayers) + actualMinutesInOven;
 }
