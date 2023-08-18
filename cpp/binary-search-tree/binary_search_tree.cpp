1fccdd7f533c24a96750531dd3f1bc8f2992aced
Victor Payno <vpayno@users.noreply.github.com>
2023-08-17 23:12:10 -0700

    c++/binary-search-tree: 1st iteration

--- cpp/binary-search-tree/binary_search_tree.cpp
+++ cpp/binary-search-tree/binary_search_tree.cpp
@@ -1,9 +1,79 @@
 #include "binary_search_tree.h"
 
-template struct binary_search_tree::binary_tree<binary_search_tree::binary_tree<unsigned int>::tree_ptr_t>;
-template struct binary_search_tree::binary_tree<binary_search_tree::binary_tree<std::string>::tree_ptr_t>;
+/*
+ * Long story short: I guess I did make things harder on my self by insisting
+ * that the proper way to write template was to separate definitions into the
+ * cpp file and declarations into the hpp file.
+ *
+ * Apparently that's not how it should be done in C or C++. Figured that out at
+ * the end when trying to figure out how to fix linking issues without including
+ * the cpp file in the test source file.
+ *
+ * I still think this isn't the same type of "hard" difficulty level as the
+ * other "hard" exercises in this track. Completing all the other exercises in
+ * the C++ track before this one doesn't prepare you for it. Compared to the
+ * rest of the exercises in the C++ track, this was a "nightmare" difficulty
+ * level.
+ *
+ * I am glad I was stubborn about separating template declarations and
+ * definitions. Learned a lot.
+ *
+ * There should have been a few
+ * - create an interator for a vector exercises before this one.
+ * - create a custom iterator for the node list you created in another exercise.
+ * - create a generator/iterator
+ * - better template exercises that help you learn about templates with classes
+ * and structs instead of being a secondary mechanic thrown in there (a lot of
+ * the solutions for those are also header file only solutions).
+ *
+ * Reference Material that helped me solve this with a cpp file with
+ * definitions and a hpp (yes for soem weird reason they keep insisting on
+ * using C header files in exercises or only a C++ source file) with only
+ * declarations.
+ *
+ * Also using clang-check (well, only if I rename the header file from *.h to
+ * *.hpp) and clang-tidy are a must. I had to restart with everything in the
+ * header file, slowly split declarations and definitions in the header file
+ * and then move the definitions to the source file. And then realizing one
+ * isn't supposed to split template declarations and definitions into multiple
+ * files. :)
+ *
+ * https://www.parashift.com/c++-faq-lite/templates.html
+ * https://isocpp.org/wiki/faq/templates
+ * https://en.cppreference.com/w/cpp/language/rule_of_three
+ * https://en.cppreference.com/w/cpp/language/move_assignment
+ * https://en.cppreference.com/w/cpp/language/move_constructor
+ * https://en.cppreference.com/w/cpp/language/copy_constructor
+ * https://en.cppreference.com/w/cpp/language/copy_assignment
+ * https://en.cppreference.com/w/cpp/language/operators
+ *
+ * https://stackoverflow.com/questions/1575181/why-does-the-operator-work-on-structs-without-having-been-defined
+ * https://stackoverflow.com/questions/3008541/template-class-symbols-not-found
+ * https://stackoverflow.com/questions/48422601/why-must-i-declare-copy-move-constructors-when-declaring-destructors
+ * https://stackoverflow.com/questions/14047191/overloading-operators-in-typedef-structs-c
+ * https://stackoverflow.com/questions/4505090/linking-templates-with-g
+ * https://stackoverflow.com/questions/115703/storing-c-template-function-definitions-in-a-cpp-file
+ *
+ * Side note: please rename all the C *.h header files to C++ *.hpp header
+ * files. The contents of the files aren't valid C, so I don't get why they're
+ * still using C header file extensions. Don't remove them. There's a lot to be
+ * learned from splitting up declarations and definitions into two separate
+ * files.
+ *
+ * The only way I can keep the definitions in the cpp file is if the test file
+ * includes the cpp file which isn't ideal. So moving the definitions back into
+ * the header file but keeping them separate because I find that easier to read
+ * and maintain.
+ */
 
-namespace binary_search_tree {
+/* this is only needed if the definitions are in this file
 
+template struct binary_search_tree::binary_tree<
+    binary_search_tree::binary_tree<unsigned int>>;
 
-}  // namespace binary_search_tree
+template struct binary_search_tree::binary_tree<
+    binary_search_tree::binary_tree<std::string>>;
+
+*/
+
+namespace binary_search_tree {} // namespace binary_search_tree
--- cpp/binary-search-tree/binary_search_tree.h
+++ cpp/binary-search-tree/binary_search_tree.h
@@ -1,212 +1,233 @@
 #if !defined(BINARY_SEARCH_TREE_H)
 #define BINARY_SEARCH_TREE_H
+#include <memory>
+#include <string>
 
-#include<iterator>
-#include<memory>
-#include<string>
-
+/*
+ * binary_search_tree - declarations
+ */
 namespace binary_search_tree {
 
-template <typename T>
-struct binary_tree {
-    public:
-        using tree_ptr_t = std::unique_ptr<binary_tree<T>>;
-        using parent_t = tree_ptr_t*;
-        using iter_t = std::input_iterator_tag;
+template <typename T> struct binary_tree {
+  public:
+    binary_tree(T data, binary_tree<T> *parent = nullptr);
+    binary_tree(binary_tree &&tree);          // move constructor
+    binary_tree(const binary_tree &tree_ref); // copy constructor
+    ~binary_tree();
 
-        // binary_tree()  = default;
-        ~binary_tree() = default;
+    using node_ptr_t = std::unique_ptr<binary_tree<T>>;
 
-        binary_tree(const T& data);
-        binary_tree(T& data, const bool& is_right = false, const bool& is_left = false);
+    binary_tree &operator=(binary_tree &&tree_ref); // move assignment operator
+    binary_tree &operator=(binary_tree rhs);        // copy assignment operator
+    bool operator<(const binary_tree &rhs);
 
-        /*
-        binary_tree(T& data, parent_t parent = nullptr, const bool& is_right = false, const bool& is_left = false);
-        binary_tree(T data, parent_t parent = nullptr, const bool& is_right = false, const bool& is_left = false);
-        binary_tree(T &data, parent_t parent = nullptr);
-        binary_tree(T data, parent_t parent = nullptr);
-        */
+    T &data();
 
-        binary_tree(std::initializer_list<T> data);
+    node_ptr_t &left();
+    node_ptr_t &right();
 
-        [[nodiscard]] T &data() ;
+    binary_tree<T> *parent();
 
-        [[nodiscard]] tree_ptr_t & parent() ;
-        [[nodiscard]] tree_ptr_t & left() ;
-        [[nodiscard]] tree_ptr_t & right() ;
+    void insert(T data);
 
-        void insert(T new_data);
+    struct iterator {
+      public:
+        iterator(binary_tree<T> *tree_ptr = nullptr);
+        iterator(const iterator &iter); // copy constructor
+        iterator(iterator &&iter);      // move constructor
+        ~iterator();
 
-        struct iterator {
-            public:
-                iterator(tree_ptr_t tree_ptr);
-                iterator(const iterator& iter);
+        iterator &operator=(binary_tree<T> *tree_ptr);
+        iterator &operator=(const iterator &iter_ref); // copy assignment op
+        iterator &operator=(iterator &&iter_ref); // move assignment operator
+        bool operator==(const iterator &rhs) const;
+        bool operator!=(const iterator &rhs) const;
+        iterator &operator++();
+        T &operator*() const;
+        const T *operator->() const;
 
-                [[nodiscard]] bool operator==(const iterator& rhs) const;
-                [[nodiscard]] bool operator!=(const iterator& rhs) const;
-                tree_ptr_t & operator++() ;
-                auto operator*() -> const iterator&;
-                auto operator->() -> const T*;
+      private:
+        binary_tree<T> *_tree_ptr{nullptr};
+    };
 
-            private:
-                tree_ptr_t _tree_ptr{nullptr};
-        };
+    auto begin() -> iterator;
+    auto end() -> iterator;
 
-        auto begin() -> iterator;
-        auto end() -> iterator;
-
-    private:
-        T _data{};
-
-        binary_tree<T>* _parent{nullptr};
-        tree_ptr_t _left{nullptr};
-        tree_ptr_t _right{nullptr};
-
-        bool _is_left{false};
-        bool _is_right{false};
+  private:
+    T _data{};
+    node_ptr_t _left{nullptr};
+    node_ptr_t _right{nullptr};
+    binary_tree<T> *_parent{nullptr};
 };
 
-    template <typename T>
-    using tree_ptr_t = std::unique_ptr<binary_tree<T>>;
+} // namespace binary_search_tree
 
-    template <typename T>
-    binary_tree<T>::binary_tree(const T& data):
-        _data(std::move(data)) {}
+/*
+ * binary_search_tree - declarations
+ */
+namespace binary_search_tree {
 
-    template <typename T>
-    binary_tree<T>::binary_tree(std::initializer_list<T> data) {
-        for(auto iter=data.begin(); iter != data.end(); iter++) {
-            insert(*iter);
-        }
-    }
+/*
+ * struct iterator definitions
+ */
 
-    template <typename T>
-    // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-    binary_tree<T>::binary_tree(T& data, const bool& is_right, const bool& is_left):
-        _data(data),
-        _parent(parent),
-        _right(is_right),
-        _left(is_left)
-    {}
+template <typename T>
+binary_tree<T>::iterator::iterator(binary_tree<T> *tree_ptr)
+    : _tree_ptr(tree_ptr) {}
 
-    /*
-    template <typename T>
-    // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-    binary_tree<T>::binary_tree(T data, parent_t parent, const bool& is_right, const bool& is_left):
-        _data(data),
-        _parent(parent),
-        _right(is_right),
-        _left(is_left)
-    {}
+template <typename T>
+binary_tree<T>::iterator::iterator(const iterator &iter)
+    : _tree_ptr(iter._tree_ptr) {}
 
-    template <typename T>
-    binary_tree<T>::binary_tree(T &data, parent_t parent):
-        _data(data),
-        _parent(parent)
-    {}
+template <typename T> binary_tree<T>::iterator::~iterator() = default;
 
-    template <typename T>
-    binary_tree<T>::binary_tree(T data, parent_t parent):
-        _data(data),
-        _parent(parent)
-    {}
-    */
+template <typename T>
+binary_tree<T>::iterator::iterator(iterator &&iter)
+    : _tree_ptr(iter._tree_ptr) {}
 
-    template <typename T>
-    T &binary_tree<T>::data() {
-        return _data;
-    }
+template <typename T>
+typename binary_tree<T>::iterator &
+binary_tree<T>::iterator::operator=(binary_tree<T> *tree_ptr) {
+    _tree_ptr = tree_ptr;
 
-    template <typename T>
-    tree_ptr_t<T> & binary_tree<T>::parent() {
-        return _parent;
-    }
+    return *this;
+}
 
-    template <typename T>
-    tree_ptr_t<T> & binary_tree<T>::left() {
-        return _left;
-    }
+template <typename T>
+typename binary_tree<T>::iterator &
+// NOLINTNEXTLINE(bugprone-unhandled-self-assignment)
+binary_tree<T>::iterator::operator=(const iterator &iter_ref) {
+    // can't figure out how to verify that this isn't equal to *iter_ref
+    _tree_ptr = iter_ref._tree_ptr;
 
-    template <typename T>
-    tree_ptr_t<T> & binary_tree<T>::right() {
-        return _right;
-    }
+    return *this;
+}
 
-    template <typename T>
-    void binary_tree<T>::insert(T new_data) {
-        if(new_data <= _data) {
-            if(_left == nullptr) {
-                _left = std::make_unique<tree_ptr_t>(new_data, this, false, true);
+template <typename T>
+typename binary_tree<T>::iterator &
+binary_tree<T>::iterator::operator=(iterator &&iter_ref) {
+    (void)iter_ref;
+
+    return *this;
+}
+
+template <typename T>
+bool binary_tree<T>::iterator::operator==(const iterator &rhs) const {
+    return _tree_ptr == rhs._tree_ptr;
+}
+
+template <typename T>
+bool binary_tree<T>::iterator::operator!=(const iterator &rhs) const {
+    return _tree_ptr != rhs._tree_ptr;
+}
+
+template <typename T>
+typename binary_tree<T>::iterator &binary_tree<T>::iterator::operator++() {
+    if (_tree_ptr->right()) {
+        _tree_ptr = _tree_ptr->right()->begin()._tree_ptr;
+    } else {
+        binary_tree<T> *grandparent = _tree_ptr->parent();
+
+        while (true) {
+            if (grandparent && (grandparent->data() < _tree_ptr->data())) {
+                grandparent = grandparent->parent();
             } else {
-                _left->insert(new_data);
-            }
-        } else {
-            if(_right == nullptr) {
-                _right = std::make_unique<tree_ptr_t>(new_data, this, false, true);
-            } else {
-                _right->insert(new_data);
+                _tree_ptr = grandparent;
+                break;
             }
         }
     }
 
-    template <typename T>
-    auto binary_tree<T>::begin() -> binary_tree<T>::iterator {
-        return (_left != nullptr) ? _left->begin() : iterator { this };
-    }
+    return *this;
+}
 
-    template <typename T>
-    auto binary_tree<T>::end() -> binary_tree<T>::iterator {
-        return iterator { nullptr };
-    }
+template <typename T> T &binary_tree<T>::iterator::operator*() const {
+    return _tree_ptr->_data;
+}
 
-    template <typename T>
-    binary_tree<T>::iterator::iterator(tree_ptr_t tree_ptr):
-        _tree_ptr(tree_ptr) {}
+template <typename T> const T *binary_tree<T>::iterator::operator->() const {
+    // const binary_search_tree::binary_tree<unsigned int>*
+    return &(operator*());
+}
 
-    template <typename T>
-    binary_tree<T>::iterator::iterator(const iterator& iter):
-        _tree_ptr(iter.tree_ptr) {}
+/*
+ * struct binary_tree definitions
+ */
 
-    template <typename T>
-    bool binary_tree<T>::iterator::operator==(const iterator& rhs) const{
-        return _tree_ptr == rhs._tree_ptr;
-    }
+template <typename T> using node_ptr_t = std::unique_ptr<binary_tree<T>>;
 
-    template <typename T>
-    bool binary_tree<T>::iterator::operator!=(const iterator& rhs) const {
-        return _tree_ptr != rhs._tree_ptr;
-    }
+template <typename T>
+binary_tree<T>::binary_tree(T data, binary_tree<T> *parent)
+    : _data(data), _left(nullptr), _right(nullptr), _parent(parent) {}
 
-    template <typename T>
-    tree_ptr_t<T> & binary_tree<T>::iterator::operator++() {
-        _tree_ptr = _tree_ptr->next();
+template <typename T>
+binary_tree<T>::binary_tree(binary_tree &&tree) // move constructor
+    : _data(tree._data) {}
 
-        return *this;
-    }
+template <typename T>
+binary_tree<T>::binary_tree(const binary_tree &tree_ref) // copy constructor
+    : _data(tree_ref._data) {}
 
-    template <typename T>
-    auto binary_tree<T>::iterator::operator*() -> const binary_tree<T>::iterator &{
-        return _tree_ptr->_data;
-    }
+template <typename T> binary_tree<T>::~binary_tree() = default;
 
-    template <typename T>
-    auto binary_tree<T>::iterator::operator->() -> const T* {
-        // return &(operator*());
-        if(_tree_ptr->right() != nullptr) {
-            _tree_ptr = _tree_ptr->right()->begin._tree_ptr;
+template <typename T>
+binary_tree<T> &binary_tree<T>::operator=(binary_tree &&tree_ref) {
+    (void)tree_ref;
+
+    return *this;
+}
+
+template <typename T>
+binary_tree<T> &binary_tree<T>::operator=(binary_tree rhs) {
+    _data = rhs._data;
+
+    return *this;
+}
+
+template <typename T> bool binary_tree<T>::operator<(const binary_tree &rhs) {
+    return _data < rhs._data;
+}
+
+template <typename T> T &binary_tree<T>::data() { return _data; }
+
+template <typename T> node_ptr_t<T> &binary_tree<T>::left() { return _left; }
+
+template <typename T> node_ptr_t<T> &binary_tree<T>::right() { return _right; }
+
+template <typename T> binary_tree<T> *binary_tree<T>::parent() {
+    return _parent;
+}
+
+// Note: The branches are "identical" because of their type, not true clones.
+// There might be a less readable way of re-writing this, not looking for it.
+// https://releases.llvm.org/11.0.0/tools/clang/tools/extra/docs/clang-tidy/checks/bugprone-branch-clone.html
+template <typename T> void binary_tree<T>::insert(T data) {
+    // NOLINTNEXTLINE(bugprone-branch-clone)
+    if (_data < data) {
+        // NOLINTNEXTLINE(bugprone-branch-clone)
+        if (_right != nullptr) {
+            _right->insert(data);
         } else {
-            binary_tree<T>* top{ _tree_ptr->parent()};
-            while(true) {
-                if(top && (top->data() < _tree_ptr->data())) {
-                    top = top->parent();
-                } else {
-                    _tree_ptr = top;
-
-                    break;
-                }
-            }
+            _right = node_ptr_t(new binary_tree<T>(data, this));
+        }
+    } else {
+        // NOLINTNEXTLINE(bugprone-branch-clone)
+        if (_left != nullptr) {
+            _left->insert(data);
+        } else {
+            _left = node_ptr_t(new binary_tree<T>(data, this));
         }
     }
-}  // namespace binary_search_tree
+}
+
+template <typename T> auto binary_tree<T>::begin() -> binary_tree<T>::iterator {
+    return (_left) ? _left->begin() : binary_tree::iterator(this);
+}
+
+template <typename T> auto binary_tree<T>::end() -> binary_tree<T>::iterator {
+    return iterator();
+}
+
+} // namespace binary_search_tree
 
 #endif // BINARY_SEARCH_TREE_H
