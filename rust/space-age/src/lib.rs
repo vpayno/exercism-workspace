ccc84f11523d9e5504b229a959264d295e2627f3
Victor Payno <vpayno@users.noreply.github.com>
2023-08-21 20:52:57 -0700

    rust/space-age: 2nd iteration - use macro

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,79 +1,70 @@
+//! Project URL: <https://exercism.org/tracks/rust/exercises/space-age>
+
 const SECONDS_IN_EARTH_YEAR: f64 = 365.25 * 24.0 * 60.0 * 60.0;
 
 #[derive(Debug)]
+/// Duration struct contains the planet's orbital period in earth seconds.
 pub struct Duration {
-    age_in_years: f64,
+    age_in_seconds: f64,
 }
 
 impl From<u64> for Duration {
+    /// from returns the planet's orbital period.
     fn from(seconds: u64) -> Self {
         Duration {
-            age_in_years: (seconds as f64) / (SECONDS_IN_EARTH_YEAR),
+            age_in_seconds: (seconds as f64) / (SECONDS_IN_EARTH_YEAR),
         }
     }
 }
 
+/// Planet trait declares the orbital_period method and calculates a person's age on a given planet.
 pub trait Planet {
     fn orbital_period() -> f64;
 
     fn years_during(duration: &Duration) -> f64 {
-        duration.age_in_years / Self::orbital_period()
+        duration.age_in_seconds / Self::orbital_period()
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+generate_planet!(Mercury, 0.2408467);
+generate_planet!(Venus, 0.61519726);
+generate_planet!(Earth, 1.0);
+generate_planet!(Mars, 1.8808158);
+generate_planet!(Jupiter, 11.862615);
+generate_planet!(Saturn, 29.447498);
+generate_planet!(Uranus, 84.016846);
+generate_planet!(Neptune, 164.79132);
 
-impl Planet for Mercury {
-    fn orbital_period() -> f64 {
-        0.2408467
-    }
-}
+/// generate_planet macro creates duplicate boiler plate (the planet structs and Planet implementations).
+/// # Examples
+///
+/// ```rust
+/// # #[macro_use]
+/// # use generate_planet;
+/// # use space_age::*;
+///
+/// # fn main() {
+/// generate_planet!(Pluto, 90_560.0/365.25);
+///
+/// let duration = Duration::from(3_000_000_000);
+///
+/// let got = Pluto::years_during(&duration);
+///
+/// let want = 0.38341676482135845;
+///
+/// assert_eq!(got, want);
+/// # }
+/// ```
+// https://blog.logrocket.com/macros-in-rust-a-tutorial-with-examples/
+#[macro_export]
+macro_rules! generate_planet {
+    ($planet:ident, $period:expr) => {
+        pub struct $planet;
 
-impl Planet for Venus {
-    fn orbital_period() -> f64 {
-        0.61519726
-    }
-}
-
-impl Planet for Earth {
-    fn orbital_period() -> f64 {
-        1.0
-    }
-}
-
-impl Planet for Mars {
-    fn orbital_period() -> f64 {
-        1.8808158
-    }
-}
-
-impl Planet for Jupiter {
-    fn orbital_period() -> f64 {
-        11.862615
-    }
-}
-
-impl Planet for Saturn {
-    fn orbital_period() -> f64 {
-        29.447498
-    }
-}
-
-impl Planet for Uranus {
-    fn orbital_period() -> f64 {
-        84.016846
-    }
-}
-
-impl Planet for Neptune {
-    fn orbital_period() -> f64 {
-        164.79132
-    }
+        impl Planet for $planet {
+            fn orbital_period() -> f64 {
+                $period
+            }
+        }
+    };
 }
