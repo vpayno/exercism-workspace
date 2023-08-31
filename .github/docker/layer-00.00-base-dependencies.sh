252e15099ce71e5ccc0dfdcdf560641fde778dc4
Victor Payno <vpayno@users.noreply.github.com>
2023-08-30 23:03:53 -0700

    ci(docker-build): add more image clean up steps to layers

--- .github/docker/layer-00.00-base-dependencies.sh
+++ .github/docker/layer-00.00-base-dependencies.sh
@@ -17,6 +17,19 @@ main() {
 	apt list --installed >/root/apt-pkgs-start.txt
 	printf "\n"
 
+	echo Running: apt remove -y "$(apt list --installed | grep -e xorg -e xserver -e qt | cut -f1 -d/)"
+	# shellcheck disable=SC2046
+	apt remove -y $(apt list --installed | grep -e xorg -e xserver -e qt | cut -f1 -d/) || exit
+	printf "\n"
+
+	echo Running: apt-get purge libx11.* libqt.*
+	apt-get purge libx11.* libqt.* || exit
+	printf "\n"
+
+	echo Running: apt-get remove -y x11-common
+	apt-get remove -y x11-common || exit
+	printf "\n"
+
 	echo apt install -y "${PACKAGES}"
 	# shellcheck disable=SC2086
 	apt install -y ${PACKAGES} || exit
