e6bccc8cca74ea9d2930797ff1faf500b1f7b20b
Victor Payno <vpayno@users.noreply.github.com>
2023-10-10 11:06:12 -0700

    ci(docker-build): update debug output from container entrypoint

--- .github/docker/entrypoint.sh
+++ .github/docker/entrypoint.sh
@@ -3,8 +3,9 @@
 # https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions
 
 # `$#` expands to the number of arguments and `$@` expands to the supplied `args`
-printf "%d args:" "$#"
+printf "Starting container entrypoint.sh:\n"
+printf "\t%d args: [" "$#"
 printf " '%s'" "$@"
-printf "\n"
+printf "]\n"
 
 "$@"
