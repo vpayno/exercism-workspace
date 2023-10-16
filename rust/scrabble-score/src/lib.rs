5b8596b03369daacb10685445df5be0b51cea63b
Victor Payno <vpayno@users.noreply.github.com>
2023-10-16 12:44:25 -0700

    rust/scrabble-score: 3rd iteration

--- rust/scrabble-score/src/lib.rs
+++ rust/scrabble-score/src/lib.rs
@@ -11,18 +11,18 @@
 /// assert_eq!(got, want);
 /// ```
 pub fn score(word: &str) -> u64 {
-    word.to_lowercase()
-        .trim()
-        .chars()
-        .map(|c| match c {
-            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
-            'd' | 'g' => 2,
-            'b' | 'c' | 'm' | 'p' => 3,
-            'f' | 'h' | 'v' | 'w' | 'y' => 4,
-            'k' => 5,
-            'j' | 'x' => 8,
-            'q' | 'z' => 10,
-            _ => 0,
-        })
-        .sum()
+    word.to_lowercase().trim().chars().map(score_letter).sum()
+}
+
+fn score_letter(letter: char) -> u64 {
+    match letter {
+        'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+        'd' | 'g' => 2,
+        'b' | 'c' | 'm' | 'p' => 3,
+        'f' | 'h' | 'v' | 'w' | 'y' => 4,
+        'k' => 5,
+        'j' | 'x' => 8,
+        'q' | 'z' => 10,
+        _ => 0,
+    }
 }
