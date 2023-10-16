6518fb321e401d58f4b754b478105c64b1efc9ce
Victor Payno <vpayno@users.noreply.github.com>
2023-10-15 17:03:25 -0700

    ruby/leap: 2nd iteration

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -4,9 +4,9 @@
 # Leap exercise
 module Year
   def self.leap?(year)
-    return true if (year % 400).zero?
-    return false if (year % 100).zero?
-    return true if (year % 4).zero?
+    return true if year.modulo(400).zero?
+    return false if year.modulo(100).zero?
+    return true if year.modulo(4).zero?
 
     false
   end
