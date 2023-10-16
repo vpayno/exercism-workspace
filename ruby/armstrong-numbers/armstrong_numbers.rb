d395e55de664b26863f22d6fc95b61039f698c11
Victor Payno <vpayno@users.noreply.github.com>
2023-10-15 21:33:25 -0700

    ruby/armstrong-numbers: 2nd iteration

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -4,25 +4,7 @@
 # Armstrong Numbers exercise
 module ArmstrongNumbers
   def self.include?(candidate)
-    return true if candidate < 10
-
-    digit_count = (Math.log10(candidate) + 1).to_i
-
-    num = candidate
-    pow_total = 0
-
-    while num.positive?
-      pow_temp = num.modulo(10)
-      pow_temp_total = 1
-
-      (0...digit_count).each do
-        pow_temp_total *= pow_temp
-      end
-
-      pow_total += pow_temp_total
-      num /= 10
-    end
-
-    pow_total == candidate
+    digits = candidate.digits
+    digits.map { |digit| digit**digits.count }.sum == candidate
   end
 end
