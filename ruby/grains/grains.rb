2d723764bf33d14fdda2a5ef84aa6db4401427d1
Victor Payno <vpayno@users.noreply.github.com>
2023-10-15 17:44:13 -0700

    ruby/grains: 2nd iteration

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -6,8 +6,12 @@ module Grains
   INDEX_MIN = 1
   INDEX_MAX = 64
 
+  def self.valid_index?(index)
+    (INDEX_MIN..INDEX_MAX).include?(index)
+  end
+
   def self.square(index)
-    raise ArgumentError, 'index must be between 1 and 64' unless (INDEX_MIN..INDEX_MAX).include?(index)
+    raise ArgumentError, 'index must be between 1 and 64' unless valid_index?(index)
 
     1 << (index - 1)
   end
