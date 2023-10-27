4defa067f53ef8ff931abc3bfc806f584688ede2
Victor Payno <vpayno@users.noreply.github.com>
2023-10-26 22:58:54 -0700

    ruby/word-count: 2nd iteration

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -5,10 +5,6 @@
 class Phrase
   def initialize(text)
     @text = text.chomp.strip
-
-    @text.chars.select! do |rune|
-      rune.letter? || rune.digit? || rune.space? || rune.apostrophe?
-    end&.join('')
   end
 
   def word_count
@@ -19,24 +15,5 @@ class Phrase
     @words.each { |word| @counts[word.downcase] += 1 }
 
     @counts
-  end
-end
-
-# Extending the String class
-class String
-  def apostrophe?
-    match?(/'/)
-  end
-
-  def space?
-    match?(/[[:space:]]/)
-  end
-
-  def letter?
-    match?(/[[:alpha:]]/)
-  end
-
-  def digit?
-    match?(/[[:digit:]]/)
   end
 end
