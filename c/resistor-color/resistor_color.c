dec9d57fd4de11a136277c06a219c492287a811e
Victor Payno <vpayno@users.noreply.github.com>
2023-11-14 21:07:23 -0800

    c/resistor-color: lint update

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -2,7 +2,7 @@
 
 resistor_band_t color_code(resistor_band_t color) { return color; }
 
-const resistor_band_t *colors() {
+const resistor_band_t *colors(void) {
     static resistor_band_t colors_list[NUMBER_OF_COLORS] = {BLACK};
 
     for (int i = 0; i < NUMBER_OF_COLORS; i++) {
