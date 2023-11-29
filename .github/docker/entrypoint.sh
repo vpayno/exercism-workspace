d0f50d5ed0a87fcf322060ad8e63a72c83802106
Victor Payno <vpayno@users.noreply.github.com>
2023-11-29 08:11:22 -0800

    ci(docker-build): source ~/.bashrc in entrypoint

--- .github/docker/entrypoint.sh
+++ .github/docker/entrypoint.sh
@@ -2,6 +2,11 @@
 
 # https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions
 
+# shellcheck disable=SC1091
+# Setup the enviroment.
+. "${HOME}/.bashrc"
+printf "\n"
+
 # `$#` expands to the number of arguments and `$@` expands to the supplied `args`
 printf "Starting container entrypoint.sh:\n"
 printf "\t%d args: [" "$#"
