8038ddaeb88642d7b50f25784269f3cc7bc30cbc
Victor Payno <vpayno@users.noreply.github.com>
2023-12-20 21:43:31 -0800

    ruby/triangle: 2nd iteration

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   def triangle?
     return false if @side_a.zero? || @side_b.zero? || @side_c.zero?
 
-    @side_a + @side_b > @side_c && @side_a + @side_c > @side_b && @side_b + @side_c > @side_a
+    @side_a + @side_b >= @side_c && @side_a + @side_c >= @side_b && @side_b + @side_c >= @side_a
   end
 
   def equilateral?
