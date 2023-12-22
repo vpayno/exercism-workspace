7cec433757038b04fb60bb7d5fed0905767667db
Victor Payno <vpayno@users.noreply.github.com>
2023-12-22 15:49:01 -0800

    lua/queen-attack: 1st iteration

--- lua/queen-attack/queen-attack.lua
+++ lua/queen-attack/queen-attack.lua
@@ -1,3 +1,19 @@
 return function(pos)
+	assert(pos.row >= 0 and pos.row < 8, "Invalid board row")
+	assert(pos.column >= 0 and pos.column < 8, "Invalid board column")
 
+	local function _slope(p1, p2)
+		return (p2.column - p1.column) / (p2.row - p1.row)
+	end
+
+	return {
+		row = pos.row,
+		column = pos.column,
+		can_attack = function(other_pos)
+			local slope = math.abs(_slope(pos, other_pos))
+
+			-- same row or diagonal or same column
+			return slope == 0.0 or slope == 1.0 or slope == math.huge
+		end,
+	}
 end
